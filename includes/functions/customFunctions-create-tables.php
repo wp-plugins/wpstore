e31a78fa18becd23a76e8bd486b890ab41c8fed4
jmorettoni <jmorettoni@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-03-19 17:39:00 +0000

    update wpstore 1.1.4
    git-svn-id: https://plugins.svn.wordpress.org/wpstore/trunk@684349 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/functions/customFunctions-create-tables.php
+++ includes/functions/customFunctions-create-tables.php
@@ -3,7 +3,7 @@
 //CREATE TABLES ----------------------------------------------------------------
    
    global $wpstore_version;
-   $wpstore_version = "1.1.2";
+   $wpstore_version = "1.1.4";
    
    //verify update version 
    
